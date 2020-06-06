810325b439ba06b699aca30b27a55022b0db765f
gavinkzy <gavin_kzy@hotmail.com>
2020-06-06 22:34:30 +0800

    Added Idle Timer.

---
+++ Law of Jungle/Assets/Scripts/IdleTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class IdleTimer : MonoBehaviour
+{
+    [SerializeField] float timeElapsed;
+    [SerializeField] TextMeshProUGUI displayTime;
+
+    string display;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayTime.text = timeElapsed.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateStopwatch();
+    }
+
+    private void UpdateStopwatch()
+    {
+        timeElapsed += Time.deltaTime;
+        display = timeElapsed.ToString("F1") + "s";
+        displayTime.text = display;
+    }
+}
