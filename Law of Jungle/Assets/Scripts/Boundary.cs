e5cce4ba2f63032bbcc0776df19200d3834b9707
gavinkzy <gavin_kzy@hotmail.com>
2020-06-12 20:01:05 +0800

    Big Update2

---
+++ Law of Jungle/Assets/Scripts/Boundary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.transform.position = new Vector2(-12.99f, 4.57f);
+    }
+}
